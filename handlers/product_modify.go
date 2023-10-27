6f9b019516d80317b8d48db5f26445ebe34a28d3
woshiLileilei <103889919+woshiLileilei@users.noreply.github.com>
2023-10-27 23:29:30 +0800

    Update product_modify.go
    Add post function

--- handlers/product_modify.go
+++ handlers/product_modify.go
@@ -1,2 +1,23 @@
 // Leilei
 package handler
+/* Catalog-buy received data */
+type catalogBuyRequest struct {
+	UserID int64 `json:"userId"`
+	ItemID int64 `json:"itemId"`
+  ProductName 
+}
+
+
+/* Catalog-add Post handle function */
+func (server *Server) catalogBuyHandler(ctx *gin.Context) {
+	fmt.Println("================================catalogBuyHandler: Start================================")
+
+	var req productAddRequest
+
+	// Read frontend data
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+	fmt.Println("UserID=", req.UserID)
+	fmt.Println("ItemID=", req.ItemID)
