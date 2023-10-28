76e8649e726a09c4cbd9a588204c9cc7857fc1d4
Merlinish <71882000+Merlinish@users.noreply.github.com>
2023-10-28 21:25:02 +0800

    Update products.sql
    something wrong with my wsl

--- db/query/products.sql
+++ db/query/products.sql
@@ -10,16 +10,12 @@ WHERE shop_owner_id = $1 LIMIT 1;
 -- name: GetProductList :many
 SELECT product_id, product_name, pic_path
 FROM products
-WHERE product_id NOT IN (SELECT *
-                         FROM products LIMIT (($1 - 1) * $2)
-    ) LIMIT $2;
+LIMIT $2
+OFFSET (($1 - 1) * $2);
 
 -- name: GetProductListByOwner :many
 SELECT product_id, product_name, pic_path
 FROM products
 WHERE shop_owner_id = $1
-  AND product_id NOT IN (SELECT *
-                         FROM products
-                         WHERE shop_owner_id = $1
-                         LIMIT (($2 - 1) * $3)
-    ) LIMIT $3;
+LIMIT $3
+OFFSET (($2 - 1) * $3);
