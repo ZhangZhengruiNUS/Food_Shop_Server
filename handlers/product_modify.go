d24c56c3138933f46f94d0801487b21f1f508e97
woshiLileilei <103889919+woshiLileilei@users.noreply.github.com>
2023-10-29 15:34:51 +0800

    Update product_modify.go

--- handlers/product_modify.go
+++ handlers/product_modify.go
@@ -2,7 +2,6 @@ package handler
 
 import (
 	db "Food_Shop_Server/db/sqlc"
-	"database/sql"
 	"fmt"
 	"net/http"
 
@@ -11,13 +10,18 @@ import (
 
 /* Product-add received data */
 type productAddRequest struct {
-	Shop_owner_id int64   `json:"Shop_owner_id"`
+	Shop_owner_id int64   `json:"shop_owner_id"`
 	Pic_path      string  `json:"pic_path"`
 	ProductInfo   string  `json:"productInfo"`
 	Price         float64 `json:"price"`
 	Quantity      int     `json:"quantity"`
-	ExpireTime    string  `json:"expireTime"`
-	Product_ID    int64
+	Create_time   string  `json:"create_time"`
+	Product_ID    int64   `json:"product_ID"`
+}
+
+type productDeleteRequest struct {
+	Shop_owner_id int64 `json:"shop_owner_id"`
+	Product_ID    int64 `json:"product_ID"`
 }
 
 /* Product-add Post handle function */
@@ -36,7 +40,7 @@ func (server *Server) productAddHandler(ctx *gin.Context) {
 	fmt.Println("ProductInfo=", req.ProductInfo)
 	fmt.Println("Price=", req.Price)
 	fmt.Println("quantity=", req.Quantity)
-	fmt.Println("ExpireTime=", req.ExpireTime)
+	fmt.Println("ExpireTime=", req.Create_time)
 	// Start database transaction
 	err := server.store.ExecTx(ctx, func(q *db.Queries) error {
 
@@ -44,31 +48,27 @@ func (server *Server) productAddHandler(ctx *gin.Context) {
 		user, err := server.store.GetUserById(ctx, req.Shop_owner_id)
 
 		// Read item's price
-		// item, err := server.store.GetItem(ctx, req.ItemID)
+		//item, err := server.store.GetItem(ctx, req.ItemID)
 
 		// Update user's credit
 
 		// Read quantity
+
+		// If ID not exists, create it
+		server.store.CreateItem(ctx, db.CreateItemParams{
+			Shop_owner_id: req.Shop_owner_id,
+			// ItemID:   req.ItemID,
+			Quantity:    req.Quantity,
+			Price:       req.Price,
+			Create_time: req.Create_time,
+			Pic_path:    req.Pic_path,
+			ProductInfo: req.ProductInfo,
+		})
 		if err != nil {
-			if err == sql.ErrNoRows {
-				// If ID not exists, create it
-				server.store.Createproduct(ctx, db.CreateproductParams{
-					Shop_owner_id: user.Shop_owner_id,
-					// ItemID:   req.ItemID,
-					Quantity:    req.Quantity,
-					Price:       req.Price,
-					ExpireTime:  req.ExpireTime,
-					Pic_path:    req.Pic_path,
-					ProductInfo: req.ProductInfo,
-				})
-				if err != nil {
-					return err
-				}
-				fmt.Println("quantity Created")
-			} else {
-				return err
-			}
+			return err
 		}
+		fmt.Println("add Created")
+
 		ctx.JSON(http.StatusOK, user)
 		return nil
 	})
@@ -86,40 +86,38 @@ func (server *Server) productAddHandler(ctx *gin.Context) {
 func (server *Server) productDeleteHandler(ctx *gin.Context) {
 	fmt.Println("================================productDeleteHandler: Start================================")
 
-	var req productAddRequest
+	var reqDelete productDeleteRequest
 
 	// Read frontend data
-	if err := ctx.ShouldBindJSON(&req); err != nil {
+	if err := ctx.ShouldBindJSON(&reqDelete); err != nil {
 		ctx.JSON(http.StatusBadRequest, errorResponse(err))
 		return
 	}
-	fmt.Println("Product_ID=", req.Product_ID)
+	fmt.Println("Product_ID=", reqDelete.Product_ID)
 
 	// Start database transaction
 	err := server.store.ExecTx(ctx, func(q *db.Queries) error {
 
 		//todo test cases in db.go
-		user, err := server.store.GetUserById(ctx, req.Shop_owner_id)
+		//user, err := server.store.GetUserById(ctx, req.Shop_owner_id)
 
 		// Read item's price
-		item, err := server.store.GetItem(ctx, req.ItemID)
+		item, err := server.store.GetItem(ctx, reqDelete.Product_ID)
 
 		// Update user's credit
 
-		// Read quantity
-		if err != nil {
-			if err == sql.ErrNoRows {
-				// If ID not exists, create it
-				server.store.Deleteproduct(ctx, item)
-				if err != nil {
-					return err
-				}
-				fmt.Println("Product Delete")
-			} else {
+		// Access control
+		if reqDelete.Shop_owner_id == item.Shop_owner_id {
+
+			// If ID not exists, create it
+			server.store.DeleteItem(ctx, reqDelete.Product_ID)
+			if err != nil {
 				return err
 			}
+			fmt.Println("Product Delete")
+
 		}
-		ctx.JSON(http.StatusOK, user)
+		ctx.JSON(http.StatusOK, reqDelete.Shop_owner_id)
 		return nil
 	})
 
